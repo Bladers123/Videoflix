2b10043490bbb851757dc7e2316861b07ec33d27
Bladers123 <tristangehring123@hotmail.de>
2025-04-04 22:16:47 +0200

    video request

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -11,31 +11,25 @@ export class VideoComponent {
   @ViewChild('videoPlayer', { static: true }) videoPlayer!: ElementRef<HTMLVideoElement>;
   @Output() close = new EventEmitter<void>();
 
+
+  videoName: string = "autofahrt";
+
   // URL des HLS-Streams
-  videoUrl = 'http://127.0.0.1:8000/api/video/index.m3u8';
+  videoUrl = 'http://127.0.0.1:8000/api/video/' + this.videoName;
   // Flag, um zu verhindern, dass der Stream mehrfach geladen wird
   isVideoLoaded = false;
 
-  // Innere, statische Klasse als CustomLoader
-  private static CustomLoader = class extends Hls.DefaultConfig.loader {
-    override load(context: any, config: any, callbacks: any): void {
-      // Falls die URL fälschlicherweise den "index.m3u8/"-Pfad enthält, entfernen
-      if (context.url.includes("index.m3u8/")) {
-        context.url = context.url.replace("index.m3u8/", "");
-      }
-      super.load(context, config, callbacks);
-    }
-  };
 
   constructor() { }
 
   // Diese Methode wird beim Klick auf das Video aufgerufen
   onVideoClick(): void {
+    console.log(this.videoUrl);
+
     if (!this.isVideoLoaded) {
       if (Hls.isSupported()) {
         const hls = new Hls({
-          autoStartLoad: false, // Stream wird erst beim Klick geladen
-          loader: VideoComponent.CustomLoader // Verwendung des inneren CustomLoaders
+          autoStartLoad: false, 
         });
         hls.loadSource(this.videoUrl);
         hls.attachMedia(this.videoPlayer.nativeElement);
