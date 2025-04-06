fc47dcd276d3cdf6e7c9b91aa092a0d784fcbe47
Bladers123 <tristangehring123@hotmail.de>
2025-04-06 23:30:46 +0200

    testing

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,14 +1,19 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, input, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import Hls from 'hls.js';
 import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common';
+
 
 
 
 @Component({
   selector: 'app-video',
+  imports: [CommonModule],
   templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss']
+  styleUrls: ['./video.component.scss'],
+
 })
+
 
 
 export class VideoComponent {
@@ -18,24 +23,28 @@ export class VideoComponent {
   @Input() title!: string;
   @Input() type!: string;
 
-
   isVideoLoaded = false;
-
+  availableLevels: any[] = [];
+  hls: Hls | null = null;
 
   constructor() { }
 
   onVideoClick(): void {
-    const videoUrl = environment.BASE_URL + environment.ENDPOINT_VIDEO + this.type + '/' + this.title;    
+    const videoUrl = environment.BASE_URL + environment.ENDPOINT_VIDEO + this.type + '/' + this.title;
     if (!this.isVideoLoaded) {
       if (Hls.isSupported()) {
-        const hls = new Hls({
-          autoStartLoad: false,
-        });
-        hls.loadSource(videoUrl);
-        hls.attachMedia(this.videoPlayer.nativeElement);
-        hls.on(Hls.Events.MANIFEST_PARSED, () => {
-          hls.startLoad();
-          this.videoPlayer.nativeElement.play();
+        this.hls = new Hls({ autoStartLoad: false });
+        this.hls.loadSource(videoUrl);
+        this.hls.attachMedia(this.videoPlayer.nativeElement);
+        this.hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          this.hls?.startLoad();
+          // Sicherheitshalber prüfen, ob nativeElement existiert
+          if (this.videoPlayer && this.videoPlayer.nativeElement) {
+            this.videoPlayer.nativeElement.play();
+          }
+          // Speichern der verfügbaren Qualitätsstufen für die UI
+          this.availableLevels = this.hls?.levels || [];
+          console.log('Verfügbare Qualitätsstufen:', this.availableLevels);
         });
       } else if (this.videoPlayer.nativeElement.canPlayType('application/vnd.apple.mpegurl')) {
         this.videoPlayer.nativeElement.src = videoUrl;
@@ -47,12 +56,25 @@ export class VideoComponent {
     }
   }
 
-  capitalizeTitle(title: string): string {
-    const replaced = title.replace(/-/g, ' ');
-    return replaced.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  // Methode zum manuellen Wechseln der Qualitätsstufe
+  changeQuality(selectedIndex: string): void {
+    const index = parseInt(selectedIndex, 10);
+    if (this.hls) {
+      // -1 = Automatische Auswahl
+      this.hls.currentLevel = index;
+      console.log('Geänderte Qualitätsstufe auf:', index);
+    }
   }
 
-  closeDialog() {
+  capitalizeTitle(title: string): string {
+    const replaced = title.replace(/-/g, ' ');
+    return replaced
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
+  closeDialog(): void {
     this.close.emit();
   }
 }
