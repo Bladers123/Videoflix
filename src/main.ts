cec7ddcd6698a78be5e8cd1beb89b5f0d66b5008
Bladers123 <tristangehring123@hotmail.de>
2025-04-28 20:28:23 +0200

    withHashLocation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
+
+    provideRouter(
+      routes,
+      withHashLocation()
+    ),
+
     ...(appConfig.providers || [])
   ]
-}).catch((err) => console.error(err));
+})
+.catch(err => console.error(err));
